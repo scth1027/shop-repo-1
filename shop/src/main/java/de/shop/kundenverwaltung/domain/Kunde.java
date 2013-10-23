92a5386b208c3192c3eb65ede780d510a147ba29
Marc Weisenburger <Marc@Marc-PC>
2013-10-23 23:07:30 +0200

    Getter, Setter

--- shop/src/main/java/de/shop/kundenverwaltung/domain/Kunde.java
+++ shop/src/main/java/de/shop/kundenverwaltung/domain/Kunde.java
@@ -2,4 +2,26 @@ package de.shop.kundenverwaltung.domain;
 
 public class Kunde {
 	long id;
+	String vorname;
+	String nachname;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getVorname() {
+		return vorname;
+	}
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+	public String getNachname() {
+		return nachname;
+	}
+	public void setNachname(String nachname) {
+		this.nachname = nachname;
+	}
+	
+	
 }
