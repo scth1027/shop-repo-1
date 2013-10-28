0ee910f00394aa45621d60eb4c896f1a16f4347e
Marc Weisenburger <Marc@Marc-PC>
2013-10-28 14:17:10 +0100

    Bestellung implementiert Enum Bestellstatus angefangen

---
+++ shop/src/main/java/de/shop/bestellverwaltung/domain/Bestellstatus.java
@@ -0,0 +1,7 @@
+package de.shop.bestellverwaltung.domain;
+
+public enum Bestellstatus {
+	
+	angenommen, versendet, storniert, bezahlt
+
+}
--- shop/src/main/java/de/shop/bestellverwaltung/domain/Bestellung.java
+++ shop/src/main/java/de/shop/bestellverwaltung/domain/Bestellung.java
@@ -1,7 +1,63 @@
 package de.shop.bestellverwaltung.domain;
 
+import java.net.URI;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+import de.shop.artikelverwaltung.domain.Artikel;
+import de.shop.kundenverwaltung.domain.Adresse;
 import de.shop.kundenverwaltung.domain.Kunde;
 
+@XmlRootElement
 public class Bestellung {
+	
+	private long id;
+	private Kunde kunde;
+	private Adresse lieferadresse;
+	//private enum bestellstatus;
+	//TODO wieso fehler?
+	
+	@XmlTransient
+	private List<Artikel> _artikel;
+	
+	private URI artikelURI;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Kunde getKunde() {
+		return kunde;
+	}
+
+	public void setKunde(Kunde kunde) {
+		this.kunde = kunde;
+	}
+
+	public Adresse getLieferadresse() {
+		return lieferadresse;
+	}
+
+	public void setLieferadresse(Adresse lieferadresse) {
+		this.lieferadresse = lieferadresse;
+	}
+
+	@Override
+	public String toString() {
+		return "Bestellung [id=" + id + ", kunde=" + kunde + ", lieferadresse="
+				+ lieferadresse + ", _artikel=" + _artikel + ", artikelURI="
+				+ artikelURI + "]";
+	}
+	
+	
+	
+	
+	
 
 }
